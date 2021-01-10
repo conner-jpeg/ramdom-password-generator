e257f8cb27ad5aa085b93bf677eaaca0a9d23e11
Conner Satyro <satyroconner@gmail.com>
2021-01-10 03:17:21 -0700

    created object for the password characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characters = {
+  "alphabet": ["a b c d e f g h i j k l m n o p q r s t u v w x y z "],
+  "numbers": ["0 1 2 3 4 5 6 7 8 9"],
+  "specialCharacters" : [" ~ ` ! @ # $ % ^ & * ( ) _ + - = {} [] \ | : ; < > , . / ? "],
+}
+console.log(characters);
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +15,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); prompt("");
+//var scale = prompt("how many characters would you like in your pass word (between 8 and 128 characters) ")
