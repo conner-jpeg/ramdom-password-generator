2b173325199b1b0b57ec957380161c7ce43c0eea
Conner Satyro <satyroconner@gmail.com>
2021-01-10 17:38:54 -0700

    sundays commits

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var characters = {
-  "alphabet": ["a b c d e f g h i j k l m n o p q r s t u v w x y z "],
-  "numbers": ["0 1 2 3 4 5 6 7 8 9"],
-  "specialCharacters" : [" ~ ` ! @ # $ % ^ & * ( ) _ + - = {} [] \ | : ; < > , . / ? "],
-};
-console.log(characters);
-var passwordLength = prompt("enter a password length between 8 and 128 characters");
+//need variable for the user selection
+var passwordLength = textLength = ">=8, <=128";
+//prompt ("please enter a password length between 8 & 128 characters long")
+//if (passwordLength === false) {}
+//var characters = {
+  //"alphabet": ["a b c d e f g h i j k l m n o p q r s t u v w x y z "],
+  //"numbers": ["0 1 2 3 4 5 6 7 8 9"],
+  //"specialCharacters" : [" ~ ` ! @ # $ % ^ & * ( ) _ + - = {} [] \ | : ; < > , . / ? "],
+//}; console.log(characters);
+var char = "abcdefghijklmnopqrstuvwxyz";
+var digits = "0,1,2,3,4,5,6,7,8,9";
+var specialR = "`~!@#$%^&*()_-=+|}{[]:;<>,./?'"
+
+console.log(26,char);
+//let passwordLength = 8 < 128; //prompt("enter a password length between 8 and 128 characters");
 //var userLength = prompt("enter a number between 8 and 128");
 //for (var passwordLength  >=8; <=128);
-if (passwordLength <=8) (passwordLength >=128); { //dunno how to set the parameter.. 
-  alert ("please enter a number between 8 and 128");
-};
-//onClick.prompt("how many characters would you like in your password (between 8 & 128)");
-console.log(passwordLength)
+//prompt ("enter a password length between 8 and 128 characters");
+
+//if (passwordLength === true) ; { //dunno how to set the parameter.. 
+  //alert ("please enter a number between 8 and 128");
+  //console.log(passwordLength)
+//}
+//else {
+ // confirm ("please reenter a number between 8 ")
+//}
+
+
+//else if (passwordLength > 128); {
+    //prompt("enter a password length between 8 and 128 characters");} studpid mother fucking js. 
+//else {}
 // Write password to the #password input
-function writePassword() {
+function writePassword(length, characters) {
   var password = generatePassword();
+  for (var i = 0; i < length; i++){
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return password;
+}
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); 
-//var scale = prompt("how many characters would you like in your pass word (between 8 and 128 characters) ")
+var generateBtn = prompt ("please enter a password length between 8 & 128 characters long");
+if (generateBtn <=8 >128){
+  alert ("welp try again")
+  prompt ("please enter a password length between 8 & 128 characters long");
+}
+else {
+  //var generateBtn = generateBtn.addEventListener("click")
+};
+
+//else {
+  //var generateBtn = generateBtn.addEventListener("click");
+
+//}}; 
